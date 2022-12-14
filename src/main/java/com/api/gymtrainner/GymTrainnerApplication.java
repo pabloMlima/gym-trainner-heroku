480257b01a2d2ecf8c8af53a1c18ce25c4f75b67
pablo <pablomlima75@gmail.com>
2022-12-14 08:31:16 -0300

    teste 6

--- src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
+++ src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
@@ -2,6 +2,8 @@ package com.api.gymtrainner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GymTrainnerApplication {
@@ -10,4 +12,11 @@ public class GymTrainnerApplication {
 		SpringApplication.run(GymTrainnerApplication.class, args);
 	}
 
+	@RestController
+	class RandomResource {
+		@GetMapping(path = "/greet")
+		public String greet() {
+			return String.format("%s says Hello from %s!", "Spring Boot", "Heroku");
+		}
+	}
 }
