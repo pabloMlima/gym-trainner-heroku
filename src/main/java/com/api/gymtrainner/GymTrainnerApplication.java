6221c0e3cfa9ab1c3ad3313abcb32b79aedcf051
pablo <pablomlima75@gmail.com>
2022-12-14 09:15:01 -0300

    teste 12

--- src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
+++ src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
@@ -2,12 +2,14 @@ package com.api.gymtrainner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GymTrainnerApplication {
 
 	public static void main(String[] args) {
