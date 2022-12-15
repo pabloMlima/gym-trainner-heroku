587f189e7705f1e2dc1c05a56981421ce3224765
pablo <pablomlima75@gmail.com>
2022-12-15 18:12:27 -0300

    commit teste

--- src/main/java/com/api/gymtrainner/controller/MusclesController.java
+++ src/main/java/com/api/gymtrainner/controller/MusclesController.java
@@ -1,19 +1,22 @@
 package com.api.gymtrainner.controller;
+import com.api.gymtrainner.services.muscles.MusclesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/api/v1/muscles")
-@RequestMapping("teste/v1")
 @RestController
+@RequestMapping("/api/v1/muscles")
 public class MusclesController {
 
+    @Autowired
+    MusclesService musclesService;
 
-    @GetMapping("/get")
-    public String teste(){
-        return "aaaaaa";
+    @GetMapping("/list")
+    public ResponseEntity<?> listMuscles(){
+        
     }
 
 }
