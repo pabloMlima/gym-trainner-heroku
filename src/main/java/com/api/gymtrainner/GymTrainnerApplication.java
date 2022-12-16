b1f1cfa0301d2f1309923b58afe33dd8a5a0e2b3
pablo <pablomlima75@gmail.com>
2022-12-16 18:54:01 -0300

    fix conflito

--- src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
+++ src/main/java/com/api/gymtrainner/GymTrainnerApplication.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-<<<<<<< HEAD
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-=======
+
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
@@ -22,7 +24,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 		 DataSourceAutoConfiguration.class,
 		 DataSourceTransactionManagerAutoConfiguration.class,
 		 HibernateJpaAutoConfiguration.class})
->>>>>>> feature_sonar_cloud
+
 public class GymTrainnerApplication {
 
 	public static void main(String[] args) {
